0f1ab640bc9405c8b5801e2d30a1bf5cfdf10297
Smarthard <th3smartchan@gmail.com>
2019-04-02 17:35:46 +0300

    Sequelize configuration added

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: null,
+    database: "myhutdb",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  test: {
+    username: "postgres",
+    password: null,
+    database: "myhutdb_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "postgres",
+    use_env_variable: 'DB_URL'
+  }
+};
