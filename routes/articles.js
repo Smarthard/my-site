6414968ef17937f5adb24ab7f0ee6f5a28d7458c
Smarthard <th3smartchan@gmail.com>
2019-04-04 00:54:42 +0300

    articles entity CRUD complete

--- routes/articles.js
+++ routes/articles.js
@@ -4,6 +4,26 @@ let router = express.Router();
 
 /* path /api/articles/ */
 
+/* CREATE */
+
+router.post('/', async (req, res) => {
+    const entity = Article.build(req.body);
+
+    entity.save().then(value => {
+        if (value) {
+            res.status(201).send(value);
+        } else {
+            res.status(500).send({message: "an error occurred during operation"});
+        }
+    }).catch(err => {
+        console.error(err);
+
+        res.status(400).send();
+    });
+});
+
+/* READ */
+
 router.get('/', async (req, res) => {
     const offset = req.query.offset || 0;
     const limit = req.query.limit || 10;
@@ -26,6 +46,34 @@ router.get('/:id', async (req, res) => {
         res.status(200).send(article);
     } else {
         res.status(404).send();
+    }
+});
+
+/* UPDATE */
+
+router.put('/:id', async (req, res) => {
+   const id = req.params.id;
+
+   Article.update(req.body, {where: {id: id}}).then(value => {
+       res.status(200).send();
+   }).catch(err => {
+       console.error(err);
+
+       res.status(400).send();
+   });
+});
+
+/* DELETE */
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    let deleted = await Article.destroy({where: {id: id}});
+
+    if (deleted) {
+        res.status(200).send({success: true, message: "article with id " + id + " removed"});
+    } else {
+        res.status(500).send({success: false, message: "wrong article id " + id});
     }
 });
 
