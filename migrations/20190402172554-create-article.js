b7b158f8828df1f54198c7ece0a9b902593544cb
Smarthard <th3smartchan@gmail.com>
2019-04-03 23:56:44 +0300

    article's name and text cannot be NULL

--- migrations/20190402172554-create-article.js
+++ migrations/20190402172554-create-article.js
@@ -9,9 +9,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       text: {
+        allowNull: false,
         type: Sequelize.TEXT
       },
       createdAt: {
