11568aef289a147fe611071594bf8bb705214403
Smarthard <th3smartchan@gmail.com>
2019-07-07 14:34:20 +0300

    DB env simplified for production

--- config/config.js
+++ config/config.js
@@ -14,11 +14,7 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    dialect: "postgres",
-    use_env_variable: 'DB_URL'
+    url: process.env.DATABASE_URL,
+    dialect: "postgres"
   }
 };
