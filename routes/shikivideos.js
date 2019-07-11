169260eb696607ead35625b83cb95e6d3e4aa7c7
Smarthard <th3smartchan@gmail.com>
2019-07-11 09:52:14 +0300

    videos API improvements

--- routes/shikivideos.js
+++ routes/shikivideos.js
@@ -8,73 +8,104 @@ let router = express.Router();
 
 /* READ */
 
-router.get('/', async (req, res) => {
-    const offset = req.query.offset;
-    const limit = req.query.limit;
-    let articles;
+router.get('/search', async (req, res) => {
+    const offset = req.query.offset || 0;
+    const limit = req.query.limit || 10;
 
-    if (limit && offset) {
-        articles = await ShikiVideos.findAll({where: {}, limit: limit, offset: offset});
+    const title = req.query.title;
+    const episode = req.query.episode;
+    const quality = req.query.quality;
+    const author = req.query.author;
+    const kind = req.query.kind;
+    const language = req.query.lang;
+    const uploader = req.query.uploader;
+
+    let sql_dyn_where = {};
+    let sql_title_where = {};
+
+    if (title) {
+        sql_title_where = {
+            [Op.or]: [
+                {anime_russian: {[Op.like]: title}},
+                {anime_english: {[Op.like]: title}}
+            ]
+        }
+    }
+
+    if (author) sql_dyn_where.author = author;
+    if (episode) sql_dyn_where.episode = episode;
+    if (kind) sql_dyn_where.kind = kind;
+    if (language) sql_dyn_where.language = language;
+    if (uploader) sql_dyn_where.uploader = uploader;
+    if (quality) sql_dyn_where.quality = quality;
+
+
+    let article = await ShikiVideos.findAll({
+        where: [
+                sql_title_where,
+                sql_dyn_where
+        ],
+        limit: limit, offset: offset
+    });
+
+    if (article) {
+        res.status(200).send(article);
     } else {
-        articles = await ShikiVideos.findOne({limit: 1, offset: 0});
+        res.status(500);
+    }
+});
+
+router.get('/:anime_id', async (req, res) => {
+    const anime_id = req.params.anime_id;
+    const offset = req.query.offset || 0;
+    const limit = req.query.limit || 10;
+
+    const episode = req.query.episode;
+    const quality = req.query.quality;
+    const author = req.query.author;
+    const kind = req.query.kind;
+    const language = req.query.lang;
+    const uploader = req.query.uploader;
+
+    let articles;
+    let sql_dyn_where = {anime_id: anime_id};
+
+    if (episode) sql_dyn_where.episode = episode;
+    if (quality) sql_dyn_where.quality = quality;
+    if (author) sql_dyn_where.author = author;
+    if (kind) sql_dyn_where.kind = kind;
+    if (language) sql_dyn_where.language = language;
+    if (uploader) sql_dyn_where.uploader = uploader;
+
+    if (!isNaN(anime_id)) {
+        articles = await ShikiVideos.findAll({
+            where: sql_dyn_where, limit: limit, offset: offset
+        });
+    } else {
+        res.status(400).send({msg: `wrong value for parameter id: ${anime_id}`});
     }
 
     if (articles) {
         res.status(200).send(articles);
     } else {
-        res.status(500).send([]);
-    }
-});
-
-router.get('/q', async (req, res) => {
-    const title = req.query.title;
-    const episode = req.query.ep;
-
-    let article;
-
-    if (title && episode) {
-        article = await ShikiVideos.findAll({
-            where: {
-                [Op.and]: [
-                    {
-                        [Op.or]: [
-                            {anime_russian: {[Op.like]: title}},
-                            {anime_english: {[Op.like]: title}}
-                        ]
-                    },
-                    {
-                        episode: episode
-                    }
-                ]
-            }
-        });
-    } else if (title) {
-        article = await ShikiVideos.findAll({
-            where: {
-                [Op.or]: [
-                    {anime_russian: {[Op.like]: title}},
-                    {anime_english: {[Op.like]: title}}
-                ]
-            }
-        });
-    } else {
-        res.status(400).send({message: `wrong value for parameter title: ${title}`})
-    }
-
-    if (article) {
-        res.status(200).send(article);
-    } else {
-        res.status(500).send([]);
+        res.status(500);
     }
 });
 
 /* OTHER */
 
-router.post('/count', (req, res) => {
-    ShikiVideos.findAndCountAll().then(value => {
-        res.status(200).send({count: value.count});
+router.get('/:anime_id/length', (req, res) => {
+    const anime_id = req.params.anime_id;
+
+    ShikiVideos.max('episode', {
+        where: {
+            anime_id: anime_id
+        }
+    }).then(value => {
+        res.status(200).send({length: value});
     }).catch(err => {
-        res.status(500).send({count: 0});
+        console.error(err);
+        res.status(500);
     });
 });
 
